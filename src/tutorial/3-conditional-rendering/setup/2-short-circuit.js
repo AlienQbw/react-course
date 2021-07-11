5755a25da8af4466a63beb77c9a2f12582b1e199
AlienQbw <wladyka.bj@gmail.com>
2021-07-11 21:01:56 +0200

    short - circuit evaluation

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+  const [text, setText] = useState("");
+  const firstValue = text || "hello world";
+  const secondValue = text && "hello world";
 
-  return <h2>short circuit</h2>;
+  return (
+    <>
+      {/* <h1>{firstValue}</h1>
+      <h1>{secondValue}</h1>
+      {} */}
+      <h1>{text ? text : `default if falsy`}</h1>
+      {!text && <h1>hello world</h1>}
+    </>
+  );
 };
 
 export default ShortCircuit;
