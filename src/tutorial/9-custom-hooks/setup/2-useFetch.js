5c7bdaa8436089430fc21ffaf29b4fd7546d4753
AlienQbw <wladyka.bj@gmail.com>
2021-09-17 21:31:22 +0200

    useCallback - Fetch Example || Course finished

--- src/tutorial/9-custom-hooks/setup/2-useFetch.js
+++ src/tutorial/9-custom-hooks/setup/2-useFetch.js
@@ -4,16 +4,20 @@ export const useFetch = (url) => {
   const [loading, setLoading] = useState(true);
   const [products, setProducts] = useState([]);
 
-  const getProducts = async () => {
+  const getProducts = useCallback(async () => {
     const response = await fetch(url);
     const products = await response.json();
     setProducts(products);
     setLoading(false);
-  };
+  });
 
-  useEffect(() => {
-    getProducts();
-  }, [url]);
+  useEffect(
+    () => {
+      getProducts();
+    },
+    [url],
+    getProducts
+  );
 
   return { loading, products };
 };
