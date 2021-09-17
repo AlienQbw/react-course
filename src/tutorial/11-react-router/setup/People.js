e801a3151d4dd1645d212f120803b7b6cdb8110b
AlienQbw <wladyka.bj@gmail.com>
2021-09-17 19:27:41 +0200

    React Router - URL Params And Placeholder

--- src/tutorial/11-react-router/setup/People.js
+++ src/tutorial/11-react-router/setup/People.js
@@ -8,8 +8,9 @@ const People = () => {
       <h1>People Page</h1>
       {people.map((person) => {
         return (
-          <div key={person.id} className='item'>
+          <div key={person.id} className="item">
             <h4>{person.name}</h4>
+            <Link to={`/person/${person.id}`}>Learn More</Link>
           </div>
         );
       })}
--- src/tutorial/11-react-router/setup/index.js
+++ src/tutorial/11-react-router/setup/index.js
@@ -25,6 +25,7 @@ const ReactRouterSetup = () => {
         <Route exact path="/people">
           <People />
         </Route>
+        <Route path="/person/:id" children={<Person />}></Route>
         <Route path="*">
           <Error />
         </Route>
